1e75310ad455f52738788ba62f3a389c4ac3bb3e
多羅狼 <Dorowolf314@gmail.com>
2026-07-21 19:09:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chess-dragon/',
+  base: process.env.NODE_ENV === 'production' ? '/chess-dragon/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
